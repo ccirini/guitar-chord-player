dc300c9ace3ac811d372698cc3c51385d5540001
ccirini <ccirini@mail.sfsu.edu>
2021-11-30 17:33:11 -0800

    changed visualizers bubble effect location and size from random to deterministic based on the tone amplitude fixed slow down caused by too many bubbles drawn per frame. changed bubble color to make the swirl pattern and particle effects more distinct

--- client/src/visualizers/ccirini.tsx
+++ client/src/visualizers/ccirini.tsx
@@ -1,6 +1,7 @@
 // 3rd party library imports
 import P5 from 'p5';
 import * as Tone from 'tone';
+import { AmplitudeEnvelope } from 'tone';
 
 import { Visualizer } from '../Visualizers';
 
@@ -22,19 +23,20 @@ export const SpinAndTrip = new Visualizer(
     p5.stroke(r,g,b);;
 
     const values = analyzer.getValue();
+
+    let particles = [];
     
-    for (let i = 0; i < values.length; i+=10) {
+    for (let i = 0; i < values.length; i+=8) {
         p5.push();
         const amplitude = values[i] as number;
-        p5.rotate((p5.frameCount +i)/100);
-        p5.rect(0,0,i+(amplitude*1000),i+(amplitude*1000),20+Math.abs(10*amplitude));
-        
-        //bubble particles
-        let point = p5.random(-height,height);
+        p5.rotate((p5.frameCount +i)/(100));
+        p5.rect(0,0,i+(amplitude*1000),i+(amplitude*1000),20);
+     
         if(amplitude!==0){
-            p5.circle(point,point,(100*amplitude));
+            p5.stroke(255,255,255);
+            particles.push(p5.circle(100+(100*amplitude),100+(100*amplitude),(100*amplitude)));
+            particles.push(p5.circle(-1*100+(100*amplitude),-1*100+(100*amplitude),(100*amplitude)));
         }
-        
         p5.pop();
     }  
   },
