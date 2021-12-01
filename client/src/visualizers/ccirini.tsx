33db2fc212410ad6beb4daaf993f90ff5ee9bf70
ccirini <ccirini@mail.sfsu.edu>
2021-11-30 17:36:46 -0800

    removed un-used imports

--- client/src/visualizers/ccirini.tsx
+++ client/src/visualizers/ccirini.tsx
@@ -1,10 +1,8 @@
 // 3rd party library imports
 import P5 from 'p5';
 import * as Tone from 'tone';
-import { AmplitudeEnvelope } from 'tone';
 
 import { Visualizer } from '../Visualizers';
-
 
 export const SpinAndTrip = new Visualizer(
   'SpinAndTrip',
