e9ce98506628bb3d3684e713e4339a2c3afc09b8
ccirini <ccirini@mail.sfsu.edu>
2022-01-19 14:44:53 -0800

    merge songs

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -15,9 +15,16 @@ CREATE TABLE songs (
     notes varchar NOT NULL
 );
 
+<<<<<<< HEAD
 INSERT INTO songs (id, bpm, musical_key, artist, song_title, notes)
 VALUES (1, 90, 'C', 'Beethoven', 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4'),
 (2, 80, 'C', 'Mozart', 'Ah! Vous dirai-je, Maman', 'G4 G4 D5 D5 E5 E5 D5 0 C5 C5 B4 B4 A4 A4 G4'),
 (3,120, 'F', 'Beethoven','Symphony No 5', 'A4 A4 A4 F4 0 0 0 G4 G4 G4 E4 0 0 0 0 A4 A4 A4 F4 Bb4 Bb4 Bb4 A4 F5 F5 F5 D5'),
 (4, 140, 'Db', 'Laurent Gelmetti','Running in the 90s', 'E4 Eb4 0 0 E4 Eb4 0 0 E4 Eb4 D4 Db4 C4 Db4 D4 Eb4 E4 Eb4 0 0 E4 Eb4 0 0 E4 Eb4 D4 Db4 C4 Db4 D4 Eb4 0'),
 (5, 100, 'G', 'Metallica', 'One', 'B3 Gb4 B3 D4 0 0 0 G3 Gb4 G3 D4 0 0 0 B3 Gb4 B3 D4 0 0 0 G3 Gb4 G3 D4 0 G4 0');
+=======
+INSERT INTO songs (id,artist,song_title, notes) 
+VALUES (1,'Beethoven','Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+INSERT INTO songs (id,artist,song_title, notes) 
+VALUES (2,'Mozart','Ah! Vous dirai-je, Maman', 'G4 G4 D4 D4 E4 E4 D4 C4 C4 B4 B4 A4 A4 G4');
+>>>>>>> parent of 8e92ebd (added more songs to db to test and implement new song sorting functionality)
